3dfd5fbd814a8b6ded6adccdcecab88349de9b1f
Marco Chiesa <marco@linux-uzmz.site>
2015-06-10 13:11:02 -0500

    Add AsyncTask to trigger pull from web service

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new ForecastFragment()).commit();
+        }
     }
 
 
